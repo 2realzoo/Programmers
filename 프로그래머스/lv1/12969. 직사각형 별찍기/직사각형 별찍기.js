8f5bbf3b643964c5643e58d29063c23078f2b9c8
2realzoo <115965399+2realzoo@users.noreply.github.com>
2022-12-16 15:09:18 +0900

    [level 1] Title: 직사각형 별찍기, Time: 49.67 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,12 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for(let i = 0 ; i < b; i ++) {
+        let stars = '';
+        for(let j = 0; j < a; j ++) {
+            stars += '*';
+        }
+        console.log(stars)
+    }
+});
